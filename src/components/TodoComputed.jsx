5ea4f52297c42afa1a5a49494d74c76f51e3c1e4
Jhon <eduardcamayo13@gmail.com>
2023-11-07 18:33:54 -0500

    Funciones de los botenes completadas

--- src/components/TodoComputed.jsx
+++ src/components/TodoComputed.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const TodoComputed = () => {
-  return (
-      <section className=" py-4 px-4 flex justify-between bg-white rounded-b-md">
-          <span className="text-gray-400">5 item left</span>
-          <button className="text-gray-400">Clear completed</button>
-      </section>
-  );
-}
+const TodoComputed = ({ computedItemsLeft, clearCompeted }) => {
+    return (
+        <section className=" py-4 px-4 flex justify-between bg-white rounded-b-md">
+            <span className="text-gray-400">{computedItemsLeft} item left</span>
+            <button className="text-gray-400" onClick={clearCompeted}>
+                Clear completed
+            </button>
+        </section>
+    );
+};
 
 export default TodoComputed
